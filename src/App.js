4183a3aea8c833209161b16b6d1f4b1c193e9af2
mohie93 <mohie_93@hotmail.com>
2019-07-07 12:50:10 +0800

    built project structuer

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React App.</h1>
+      <br/>
+      <p>Copy rights @Mohie93 - 2019</p>
+    </div>
+  );
+}
+
+export default App;
