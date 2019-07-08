578ec10b54098a16c3aac0d8494863f4781b062b
mohie93 <mohie_93@hotmail.com>
2019-07-08 14:54:53 +0800

    updated comments service

--- src/services/CommentService.js
+++ src/services/CommentService.js
@@ -4,12 +4,12 @@ export default class CommentService {
 
     host = process.env.REACT_APP_HOST;
     port = process.env.REACT_APP_PORT;
-    prefix = 'bulletin';
+    prefix = 'comment';
     baseURL = `${host}:${port}/${prefix}`;
 
     all = async (bulletinId) => {
         try {
-            const response = await axios.get('',bu);
+            const response = await axios.get(`${baseURL}/show/${bulletinId}`);
             const comments = response.status !== 200 ? [] : response.data.comments ;
             return comments;   
         } catch (error) {
@@ -20,7 +20,7 @@ export default class CommentService {
 
     create = async (bulletinId, content) => {
         try {
-            const response = await axios.post('',{bulletinId, content});
+            const response = await axios.post(`${baseURL}/create`,{bulletinId, content});
             const comment = response.status !== 201 ? {} : response.data.comment ;
             return comment;   
         } catch (error) {
