8554ee613aad7f70bb3476e056c156993e845b81
mohie93 <mohie_93@hotmail.com>
2019-07-08 15:00:35 +0800

    added no bulletins error handleing

--- src/components/Bulletins.js
+++ src/components/Bulletins.js
@@ -3,9 +3,15 @@ import Bulletin from './BulletinItem';
 
 export class Bulletins extends Component {
     render() {
-        return this.props.bulletins.map( (bulletin) => (
-            <Bulletin bulletin={bulletin} key={bulletin.id}/>
-        ))
+        if (this.props.bulletins.length > 0) {
+            return this.props.bulletins.map( (bulletin) => (
+                <Bulletin bulletin={bulletin} key={bulletin.id}/>
+            ));
+        }
+        return (
+            <h1>"Oh snap!, No bulletins to display."</h1>
+        )
+
     }
 }
 
